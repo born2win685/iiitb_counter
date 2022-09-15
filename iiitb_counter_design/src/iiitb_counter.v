eb3ad88060ef6f7631afe226594f3e935e87c136
Sathiya Naraayanan <77498786+born2win685@users.noreply.github.com>
2022-09-15 15:57:29 +0530

    Add files via upload

---
+++ iiitb_counter_design/src/iiitb_counter.v
@@ -0,0 +1,38 @@
+module iiitb_counter(RN,CK,Qz,Qu,Q);
+input RN,CK;
+output [3:0] Qz;
+output [3:0] Qu;
+output [7:0] Q;
+reg  [3:0] Qz1,Qu1;
+
+always @ (posedge CK or negedge RN)
+begin
+if(!RN )   begin
+           Qz1=4'b0;
+           Qu1=4'b0;
+           end
+else
+begin
+    if ({Qz1,Qu1}==8'b10011001) 
+    begin
+      Qz1=4'b0;
+      Qu1=4'b0;
+    end
+    else if  (Qu1==4'b1001) begin
+      Qu1=4'b0;
+      Qz1= Qz1 +1;
+
+      if(Qz1==4'b1001)
+          Qz1=4'b0;
+      end
+    else
+      Qu1=Qu1+1;
+end
+end
+
+assign Q={Qz1,Qu1};
+assign Qu=Qu1;
+assign Qz=Qz1;
+
+endmodule
+
